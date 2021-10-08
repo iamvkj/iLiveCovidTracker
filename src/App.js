b428c3bc265ea6659fb38296cdd0e7635e7fab9c
Vijayant Joshi <85673559+iamvkj@users.noreply.github.com>
2021-10-08 17:02:50 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Statewise from './components/Statewise';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import "./App.css";
+
+const App = () => {
+    return (
+        <>
+            <Statewise />
+        </>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Statewise from './StateWise/Statewise';
-import '../src/StateWise/statewise.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import App from "./App";
 
-ReactDOM.render(
-    <Statewise />,
-    document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
